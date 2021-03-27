47fcee3470d272acca81693c2ef250cb74594699
GitHub Action <action@github.com>
2021-03-27 10:35:14 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/selcukguvel/jsonviewplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.selcukguvel.jsonviewplugin.services
+
+import com.github.selcukguvel.jsonviewplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/selcukguvel/jsonviewplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/selcukguvel/jsonviewplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.selcukguvel.jsonviewplugin.services
 
+import com.github.selcukguvel.jsonviewplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
