b5565bba1880babe209314f1797352ed5a9eeef6
selcukguvel <selcukguvel@gmail.com>
2021-04-14 01:25:14 +0300

    Add mockito as dependency for testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,7 @@ repositories {
 }
 dependencies {
     detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.15.0")
+    testImplementation("org.mockito:mockito-core:1.+")
 }
 
 // Configure gradle-intellij-plugin plugin.
